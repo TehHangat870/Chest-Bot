84a00d1f5b6fd1f7f5d571c094f2abd4994ddacd
Es Teh <brandonandrew870@gmail.com>
2022-01-02 14:33:00 +0700

    Makes userinfo able to target other users

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -5,17 +5,23 @@ const { color } = require('./config.json');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('userinfo')
-		.setDescription('Replies with your user info!'),
+		.setDescription('Replies with the user info of the target!')
+		.addUserOption(option =>
+			option.setName('target')
+				.setDescription('The user you want to get the info of.')
+				.setRequired(true)),
 	async execute(interaction) {
+		const target = interaction.options.getUser('target');
+
 		const userInfo = new MessageEmbed()
 		.setColor(color)
 		.setTitle("User Info")
 		.addFields(
-			{ name: 'Username', value: String(interaction.user.username), inline: true },
-			{ name: 'User ID', value:  String(interaction.user.id), inline: true },
-			{ name: 'User Tag', value:  String(interaction.user.tag), inline: true },
-			{ name: 'User Created At', value:  String(interaction.user.createdAt)},
-			{ name: 'User Avatar URL', value:  "https://cdn.discordapp.com/avatars/"+interaction.user.id+"/"+interaction.user.avatar+".jpeg"},
+			{ name: 'Username', value: String(target.user.username), inline: true },
+			{ name: 'User ID', value:  String(target.user.id), inline: true },
+			{ name: 'User Tag', value:  String(target.user.tag), inline: true },
+			{ name: 'User Created At', value:  String(target.user.createdAt)},
+			{ name: 'User Avatar URL', value:  "https://cdn.discordapp.com/avatars/"+target.user.id+"/"+target.user.avatar+".jpeg"},
 		)
 		await interaction.reply({ embeds: [userInfo] })
 	},
